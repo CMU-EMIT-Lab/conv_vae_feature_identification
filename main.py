07794471c916ddc5952e14a255f63730ea315624
wfrieden <99196046+wfrieden@users.noreply.github.com>
2023-01-25 16:11:02 -0500

    fixes

--- main.py
+++ main.py
@@ -1,14 +1,13 @@
-import tensorflow as tf
-import matplotlib
-from utils import check_dir
 from model import CVAE, Encoder, Decoder
+from randomforest import *
+from utils import *
 
 print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
 print('Tensorflow: %s' % tf.__version__)  # print version
-matplotlib.style.use('ggplot')
 
 new_model = True
-parent_dir = 'HighCycleLowCycleNoBorder_Regime'
+find_dimensions = True
+parent_dir = 'data_binary_watermark'
 sub_dir = 'my_model'
 
 
@@ -48,25 +47,42 @@ if __name__ == "__main__":
         check_params = TrainParams(
             parent_dir=parent_dir,
             name=sub_dir,
-            epochs=2,
+            epochs=20,
             batch_size=16,
-            image_size=128,
+            image_size=32,
             latent_dim=32,
             num_examples_to_generate=16,
             learning_rate=0.001
             # show_latent_gif=True
         )
         cvae, test_ds, train_ds = train_a_model(check_params)
-    else:
-        load_params = TrainParams(
-            parent_dir=parent_dir,
-            name=sub_dir,
-            epochs=2,
-            batch_size=16,
-            image_size=128,
-            latent_dim=32,
-            num_examples_to_generate=16,
-            learning_rate=0.001
-            # show_latent_gif=True
+    # else:
+    #     load_params = TrainParams(
+    #         parent_dir=parent_dir,
+    #         name=sub_dir,
+    #         epochs=2,
+    #         batch_size=16,
+    #         image_size=128,
+    #         latent_dim=32,
+    #         num_examples_to_generate=16,
+    #         learning_rate=0.001
+    #         # show_latent_gif=True
+    #     )
+    #     cvae, test_ds, train_ds = load_data(parent_dir)
+
+    if find_dimensions:
+        # Get arrays of encoded data from model
+        train_encodings, train_labels, train_files, split_train_encodings, _ = get_encoding(cvae, train_ds)
+        test_encodings, test_labels, test_files, _, _ = get_encoding(cvae, test_ds)
+
+        # Run arrays through random forest regression to figure out if any can separate the labels
+        valuable_encodings, forest_model = random_forest(
+            train_encodings,
+            train_labels,
+            test_encodings,
+            test_labels,
+            check_params
         )
-        test_ds, train_ds = load_data(parent_dir)
+        show_split(split_train_encodings, valuable_encodings, forest_model, check_params)
+        save_tree(forest_model, check_params)
+
